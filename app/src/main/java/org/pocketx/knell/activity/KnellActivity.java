d3f03b37ea19458a93007ddca20c577b0ee273ac
dzzchao <dzzchao@163.com>
2018-05-27 16:12:18 +0800

    ugly clock version 1

--- app/src/main/java/org/pocketx/knell/activity/KnellActivity.java
+++ app/src/main/java/org/pocketx/knell/activity/KnellActivity.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.widget.TextView;
 
+import org.apache.commons.lang3.concurrent.BasicThreadFactory;
 import org.pocketx.knell.R;
 import org.pocketx.knell.base.BaseActivity;
 import org.pocketx.knell.domain.Birthday;
+import org.pocketx.knell.view.ClockView;
 import org.threeten.bp.Duration;
 import org.threeten.bp.LocalDateTime;
 import org.threeten.bp.temporal.ChronoUnit;
+
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -27,6 +33,7 @@ import timber.log.Timber;
 
 public class KnellActivity extends BaseActivity {
 
+
     @BindView(R.id.years)
     TextView mYearsView;
     @BindView(R.id.months)
@@ -39,6 +46,10 @@ public class KnellActivity extends BaseActivity {
     TextView mHoursView;
     @BindView(R.id.minutes)
     TextView mMinutesView;
+    @BindView(R.id.clock_view)
+    ClockView mClockView;
+
+    private ScheduledExecutorService mExecutorService;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -46,6 +57,69 @@ public class KnellActivity extends BaseActivity {
         setContentView(R.layout.activity_knell);
         ButterKnife.bind(this);
 
+
+        //偶然逛知乎看到的，2333
+        Timber.d("dog face\n"+ "─────────▄──────────────▄──── \n"+
+        " ─ wow ──▌▒█───────────▄▀▒▌─── \n"+
+        " ────────▌▒▒▀▄───────▄▀▒▒▒▐─── \n"+
+        " ───────▐▄▀▒▒▀▀▀▀▄▄▄▀▒▒▒▒▒▐─── \n"+
+        " ─────▄▄▀▒▒▒▒▒▒▒▒▒▒▒█▒▒▄█▒▐─── \n"+
+        " ───▄▀▒▒▒▒▒▒ such difference ─ \n"+
+        " ──▐▒▒▒▄▄▄▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄▒▒▌── \n"+
+        " ──▌▒▒▐▄█▀▒▒▒▒▄▀█▄▒▒▒▒▒▒▒█▒▐── \n"+
+        " ─▐▒▒▒▒▒▒▒▒▒▒▒▌██▀▒▒▒▒▒▒▒▒▀▄▌─ \n"+
+        " ─▌▒▀▄██▄▒▒▒▒▒▒▒▒▒▒▒░░░░▒▒▒▒▌─ \n"+
+        " ─▌▀▐▄█▄█▌▄▒▀▒▒▒▒▒▒░░░░░░▒▒▒▐─ \n"+
+        " ▐▒▀▐▀▐▀▒▒▄▄▒▄▒▒▒ electrons ▒▌ \n"+
+        " ▐▒▒▒▀▀▄▄▒▒▒▄▒▒▒▒▒▒░░░░░░▒▒▒▐─ \n"+
+        " ─▌▒▒▒▒▒▒▀▀▀▒▒▒▒▒▒▒▒░░░░▒▒▒▒▌─ \n"+
+        " ─▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐── \n"+
+        " ──▀ amaze ▒▒▒▒▒▒▒▒▒▒▒▄▒▒▒▒▌── \n"+
+        " ────▀▄▒▒▒▒▒▒▒▒▒▒▄▄▄▀▒▒▒▒▄▀─── \n"+
+        " ───▐▀▒▀▄▄▄▄▄▄▀▀▀▒▒▒▒▒▄▄▀───── \n"+
+        " ──▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▀──────── \n"
+				);
+
+
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        //创建计时器来刷新文字
+        mExecutorService = new ScheduledThreadPoolExecutor(1,
+                new BasicThreadFactory.Builder().namingPattern("Knell-scheduled").daemon(true).build());
+        mExecutorService.scheduleAtFixedRate(new Runnable() {
+            @Override
+            public void run() {
+                refresh();
+            }
+        }, 0, 1000, TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mExecutorService.shutdownNow();
+    }
+
+    /**
+     * 刷新时间显示
+     */
+    private void refresh() {
+        //时钟显示
+        LocalDateTime now = LocalDateTime.now();
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mClockView.setClockData(now.getHour(), now.getMinute(),now.getSecond() );
+            }
+        });
+
+
+        //文字显示
         Birthday birthday = getBirthdayManager().get();
         LocalDateTime dateTime = LocalDateTime.of(birthday.year, birthday.month,
                 birthday.day, 0, 0);
@@ -60,12 +134,17 @@ public class KnellActivity extends BaseActivity {
         Timber.d("onCreate: 年：%d 月：%d 周：%d 天数为：%d 小时数为：%d 分钟数为：%d",
                 years, months, weeks, days, hours, minutes);
 
-        mYearsView.setText(String.format("%d\n年", years));
-        mMonthsView.setText(String.format("%d\n月", months));
-        mWeeksView.setText(String.format("%d\n周", weeks));
-        mDaysView.setText(String.format("%d\n日", days));
-        mHoursView.setText(String.format("%d\n时", hours));
-        mMinutesView.setText(String.format("%d\n分", minutes));
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mYearsView.setText(String.format("%d\n年", years));
+                mMonthsView.setText(String.format("%d\n月", months));
+                mWeeksView.setText(String.format("%d\n周", weeks));
+                mDaysView.setText(String.format("%d\n日", days));
+                mHoursView.setText(String.format("%d\n时", hours));
+                mMinutesView.setText(String.format("%d\n分", minutes));
+            }
+        });
     }
 
     /**
@@ -77,6 +156,7 @@ public class KnellActivity extends BaseActivity {
         startActivity(new Intent(this, WelcomeActivity.class));
         finish();
     }
+
 }
 
 
